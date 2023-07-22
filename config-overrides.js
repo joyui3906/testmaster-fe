0755ea55eb366429189964c1814454ad741713ce
joyui <seclusionor@gmail.com>
2023-07-22 23:30:13 +0900

    [tmf-002] gnb 추가하기
    - 설정 오버라이드 파일 수정

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,15 @@
-const {override, addWebpackAlias} = require('customize-cra');
+const { override, addWebpackAlias } = require('customize-cra');
 const path = require('path');
 
 module.exports = override(
-    addWebpackAlias({
-        ['@src']: path.resolve(__dirname, './src'),
-        ['@const']: path.resolve(__dirname, './src/constants'),
-        ['@assets']: path.resolve(__dirname, './src/assets'),
-        ['@mocks']: path.resolve(__dirname, './apis/mocks'),
-        ['@apis']: path.resolve(__dirname, './apis'),
-        ['@styles']: path.resolve(__dirname, './src/styles'),
-        ['@static']: path.resolve(__dirname, './src/static'),
-        ['@axios']: path.resolve(__dirname, './src/utils/axios'),
-    }),
+  addWebpackAlias({
+    ['@src']: path.resolve(__dirname, './src'),
+    ['@const']: path.resolve(__dirname, './src/constants'),
+    ['@assets']: path.resolve(__dirname, './src/assets'),
+    ['@mocks']: path.resolve(__dirname, './apis/mocks'),
+    ['@apis']: path.resolve(__dirname, './apis'),
+    ['@styles']: path.resolve(__dirname, './src/styles'),
+    ['@static']: path.resolve(__dirname, './src/static'),
+    ['@axios']: path.resolve(__dirname, './src/utils/axios'),
+  }),
 );
