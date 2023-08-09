b4c32eb094a70865aa43d2ae7bde52520ffd291a
joyui <seclusionor@gmail.com>
2023-08-09 23:53:13 +0900

    [tmf-006] 회원 가입 페이지 상세화
    - 회원 가입 페이지 상세화
    - 남은 작업 : 선생, 학생 구분에 따른 드롭다운 컴포넌트 보여주기 변경?

--- src/pages/register/RegisterPage.tsx
+++ src/pages/register/RegisterPage.tsx
@@ -1,10 +1,20 @@
 import styled from '@emotion/styled';
-import {Box, Button, Container, Paper, TextField, Typography} from '@mui/material';
-import { PATH } from '@src/constants/path';
-import useInterval from '@src/hooks/useInterval';
+import {
+    Box,
+    Button,
+    Container,
+    Divider,
+    FormControl,
+    FormControlLabel,
+    Radio,
+    RadioGroup,
+    Stack,
+    TextField,
+    Typography
+} from '@mui/material';
 import {ChangeEvent, FormEventHandler, useState} from 'react';
-import {Link, useNavigate} from 'react-router-dom';
 import Page from "@src/components/common/page/Page";
+import {HeaderBlock} from './RegisterPage.style';
 
 const ContentStyle = styled.div`
   maxWidth: 480,
@@ -17,10 +27,10 @@ const ContentStyle = styled.div`
 `;
 
 interface Page404Props {
-  delayTimeMills?: number;
+    delayTimeMills?: number;
 }
 
-const RegisterPage = ({ delayTimeMills = 0 }: Page404Props) => {
+const RegisterPage = ({delayTimeMills = 0}: Page404Props) => {
 
     const [formData, setFormData] = useState({
         email: '',
@@ -29,7 +39,7 @@ const RegisterPage = ({ delayTimeMills = 0 }: Page404Props) => {
     });
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
+        const {name, value} = e.target;
         setFormData((prevData) => ({
             ...prevData,
             [name]: value,
@@ -40,54 +50,186 @@ const RegisterPage = ({ delayTimeMills = 0 }: Page404Props) => {
 //        e.preventDefault();
         console.log(formData); // 이곳에서 회원 가입 로직을 추가하면 됩니다.
     };
-  return (
-    <Page>
-      <Box>
-          <Container component="main" maxWidth="xs">
-                  <Typography component="h1" variant="h5">
-                      회원 가입
-                  </Typography>
-                      <TextField
-                          variant="outlined"
-                          margin="normal"
-                          fullWidth
-                          label="이메일"
-                          name="email"
-                          value={formData.email}
-                          onChange={handleChange}
-                          autoFocus
-                      />
-                      <TextField
-                          variant="outlined"
-                          margin="normal"
-                          fullWidth
-                          label="비밀번호"
-                          name="password"
-                          type="password"
-                          value={formData.password}
-                          onChange={handleChange}
-                      />
-                      <TextField
-                          variant="outlined"
-                          margin="normal"
-                          fullWidth
-                          label="이름"
-                          name="name"
-                          value={formData.name}
-                          onChange={handleChange}
-                      />
-                      <Button
-                          type="submit"
-                          fullWidth
-                          variant="contained"
-                          color="primary"
-                      >
-                          회원 가입
-                      </Button>
-          </Container>
-      </Box>
-    </Page>
-  );
+    return (
+        <Page>
+            <Container maxWidth={false}>
+                <Box>
+                    <HeaderBlock>
+                        <Typography component="h1" variant="h5">
+                            회원 정보 입력
+                        </Typography>
+                    </HeaderBlock>
+                </Box>
+                <Stack spacing={2}>
+                    <Stack
+                        sx={{pt: 1}}
+                        direction='row'
+                        divider={<Divider orientation="vertical" flexItem></Divider>}
+                        alignItems={"center"}
+                        spacing={2}
+                    >
+                        <Typography width='20%' sx={{flexShrink: 0}}>
+                            회원 정보 구분
+                        </Typography>
+                        <FormControl sx={{pl: 2}}>
+                            <RadioGroup sx={{justifyContent: "space-between"}} row name="userType"
+                                        onChange={handleChange}>
+                                <FormControlLabel value="student" control={<Radio/>} label="학생"/>
+                                <FormControlLabel value="teacher" control={<Radio/>} label="선생님"/>
+                            </RadioGroup>
+                        </FormControl>
+                    </Stack>
+                    <Stack direction='row'
+                           divider={<Divider orientation="vertical" flexItem></Divider>}
+                           alignItems={"center"}
+                           spacing={2}
+                    >
+                        <Typography width='20%' sx={{flexShrink: 0}}>
+                            학교
+                        </Typography>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            label="비밀번호"
+                            fullWidth
+                            name="password"
+                            type="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                        />
+                    </Stack>
+                    <Stack direction='row'
+                           divider={<Divider orientation="vertical" flexItem></Divider>}
+                           alignItems={"center"}
+                           spacing={2}
+                    >
+                        <Typography width='20%' sx={{flexShrink: 0}}>
+                            이름
+                        </Typography>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            label="이름"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                        />
+                    </Stack>
+                    <Stack direction='row'
+                           divider={<Divider orientation="vertical" flexItem></Divider>}
+                           alignItems={"center"}
+                           spacing={2}
+                    >
+                        <Typography width='20%' sx={{flexShrink: 0}}>
+                            성별
+                        </Typography>
+                        <FormControl sx={{pl: 2}}>
+                            <RadioGroup sx={{justifyContent: "space-between"}} row name="userType"
+                                        onChange={handleChange}>
+                                <FormControlLabel value="M" control={<Radio/>} label="남"/>
+                                <FormControlLabel value="F" control={<Radio/>} label="여"/>
+                            </RadioGroup>
+                        </FormControl>
+                    </Stack>
+                    <Stack direction='row'
+                           divider={<Divider orientation="vertical" flexItem></Divider>}
+                           alignItems={"center"}
+                           spacing={2}
+                    >
+                        <Typography width='20%' sx={{flexShrink: 0}}>
+                            아이디
+                        </Typography>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            label="아이디"
+                            fullWidth
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                        />
+                    </Stack>
+                    <Stack direction='row'
+                           divider={<Divider orientation="vertical" flexItem></Divider>}
+                           alignItems={"center"}
+                           spacing={2}
+                    >
+                        <Typography width='20%' sx={{flexShrink: 0}}>
+                            비밀번호
+                        </Typography>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            label="비밀번호"
+                            fullWidth
+                            name="password"
+                            type="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                        />
+                    </Stack>
+                    <Stack direction='row'
+                           divider={<Divider orientation="vertical" flexItem></Divider>}
+                           alignItems={"center"}
+                           spacing={2}
+                    >
+                        <Typography width='20%' sx={{flexShrink: 0}}>
+                            헨드폰 번호
+                        </Typography>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            label="핸드폰 번호"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                        />
+                    </Stack>
+                    <Stack direction='row'
+                           divider={<Divider orientation="vertical" flexItem></Divider>}
+                           alignItems={"center"}
+                           spacing={2}
+                    >
+                        <Typography width='20%' sx={{flexShrink: 0}}>
+                            이메일
+                        </Typography>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            label="이메일"
+                            name="name"
+                            type="email"
+                            value={formData.name}
+                            onChange={handleChange}
+                        />
+                    </Stack>
+                    <Stack direction={"row"} spacing={1}>
+                        <Button
+                            type="button"
+                            fullWidth
+                            variant="outlined"
+                            color="primary"
+                        >
+                            취소
+                        </Button>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                        >
+                            회원 가입
+                        </Button>
+                    </Stack>
+                </Stack>
+
+
+            </Container>
+        </Page>
+    );
 };
 
 export default RegisterPage;
