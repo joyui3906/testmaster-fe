17df751737bfadedbcddeb8613e5b4e3d7bdb1e0
joyui <seclusionor@gmail.com>
2023-08-09 23:56:18 +0900

    [tmf-007] 회원 가입 페이지 상세화
    - 회원 가입 페이지 상세화
    - 남은 작업 : 선생, 학생 구분에 따른 드롭다운 컴포넌트 보여주기 변경?

---
+++ src/pages/register/RegisterPage.style.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+// HeaderBlock 스타일 컴포넌트
+export const HeaderBlock = styled.div`
+  background-color: black;
+  color: white;
+  padding: 10px;
+  text-align: center;
+`;
