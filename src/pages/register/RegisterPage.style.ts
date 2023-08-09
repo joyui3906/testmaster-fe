178ef202ff2bb3b2cee7cca879614ed8edd3c976
joyui <seclusionor@gmail.com>
2023-08-09 23:53:40 +0900

    Revert "[tmf-006] 회원 가입 페이지 상세화"
    This reverts commit b4c32eb094a70865aa43d2ae7bde52520ffd291a.

--- src/pages/register/RegisterPage.style.ts
+++
@@ -1,9 +0,0 @@
-// HeaderBlock 스타일 컴포넌트
-import styled from "@emotion/styled";
-
-export const HeaderBlock = styled('div')`
-  background-color: black;
-  color: white;
-  padding: 10px;
-  text-align: left;
-`;
